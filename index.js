bbfd4beb56edcdba68ea9fffa97654fac235b4d2
Eamon Sheil <Eamondsheil@gmail.com>
2022-01-26 10:58:05 -0500

    commit tree 1fafdaefb23c186edc7477fbabfa7a039b34a715

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+return string.toUpperCase();
+}
+shout("helllo");
+
+function whisper(string) {
+    return string.toLowerCase();
+    }
+whisper("HELLO");
+
+function logShout(string) {
+    console.log(`${string.toUpperCase(string)}`);
+}
+logShout("hello");
+
+function logWhisper(string) {
+    console.log(`${string.toLowerCase(string)}`);
+}
+logWhisper("hello");
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!";
+    }
+    if  (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    if  (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
